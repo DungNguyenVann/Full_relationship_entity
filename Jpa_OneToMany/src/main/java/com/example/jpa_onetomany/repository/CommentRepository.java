549064169ea78fd7db99eebc4488484f95ac62e9
A39384 <75817960+A39384@users.noreply.github.com>
2023-10-12 00:06:07 +0700

    oneToOne, oneToMany, manyToMany

---
+++ Jpa_OneToMany/src/main/java/com/example/jpa_onetomany/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.example.jpa_onetomany.repository;
+
+import com.example.jpa_onetomany.model.Comment;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByTutorialId(Long postId);
+
+    @Transactional
+    void deleteByTutorialId(long tutorialId);
+}
